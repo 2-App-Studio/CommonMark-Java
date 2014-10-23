3b5e86507e132f42d2bdb6d09b07fe8e2fe14b62
Yap Wei Rong <kennyrong@gmail.com>
2014-10-23 14:41:40 +0800

    Added Apache Commons Lang3 lib + Removed EntityTrans

--- CommonMark-Java-Src/src/com/jotterpad/commonmark/test/HtmlRendererTest.java
+++ CommonMark-Java-Src/src/com/jotterpad/commonmark/test/HtmlRendererTest.java
@@ -1,12 +1,22 @@
 package com.jotterpad.commonmark.test;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.junit.Rule;
+import org.junit.Test;
 
 public class HtmlRendererTest {
+	
 	@Rule
 	public ResourceFile resIn = new ResourceFile("/spec.txt");
 
 	@Rule
 	public ResourceFile resOut = new ResourceFile("/spec.html");
 
+	@Test
+	public void test() {
+		System.out.println(StringEscapeUtils.unescapeHtml4("&amp;"));
+		System.out.println(StringEscapeUtils.escapeHtml4("&"));
+		// escape: & => &amp;
+		// unescape: &amp; => &
+	}
 }
